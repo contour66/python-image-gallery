df56e11438ca853ddc0d48017bcc8c835869975f
contour66 <zamir.thind@gmail.com>
2020-06-14 22:50:53 -0700

    jinja flask html

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-from tools.user_admin.py import print_names()
+from tools.user_admin import print_names()
 app = Flask(__name__)
 
 
