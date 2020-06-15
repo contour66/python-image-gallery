0a4eb661f2ca93db98a3199c8663eb001fe56a89
contour66 <zamir.thind@gmail.com>
2020-06-14 19:45:05 -0700

    flask

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Zum!'
+    return "hello world!",
 
 @app.route('/admin')
 def admin():
     return 'admin'
+
+@app.route('/admin/<name>')
+def admin(name):
+    return 'Nice to meet you ' + name
