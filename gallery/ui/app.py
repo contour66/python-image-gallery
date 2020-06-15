d0f9939500e19eb6a5c1af1c7f7e0f0e183b0ebb
contour66 <zamir.thind@gmail.com>
2020-06-14 19:57:34 -0700

    calculator

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -13,3 +13,30 @@ def admin():
 @app.route('/greet/<name>')
 def greet(name):
     return 'Nice to meet you ' + name
+
+@app.route('/mult')
+def mult():
+    x = request.args['x']
+    y = request.args['y']
+    return 'The product is ' + str[int(x)*int(y)]
+
+@app.route('/calculator')
+def calculator():
+        return """
+        <!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Caluclator</title>
+	
+</head>
+<body>
+	<form>
+		x: <input value="0" name="x"/><br>
+		y: <input value="o" name="y"><br>
+		<input type="submit" value="Multiply"/>
+
+	</form>
+</body>
+</html>
+"""
