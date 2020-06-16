d5a0a3b58a51933e2925e82925bf4e8a90e1e0b5
contour66 <zamir.thind@gmail.com>
2020-06-15 22:21:29 -0700

    adduser.html and add_user_ui

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -8,32 +8,10 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return print_names()
-
-
-# return "hello world!"
-
-
 @app.route('/admin')
 def adminPage():
     data = print_names()
     return render_template('admin.html', results=data)
-
-
-@app.route('/greet/<name>')
-def greet(name):
-    return 'Nice to meet you ' + name
-
-
-# @app.route('/add/<int:x>/<int:y>', methods=['GET'])
-# def add(x, y):
-#     return 'The sum is ' + str(x + y)
-#
-@app.route('/calculator/<personsName>')
-def calculator(personsName):
-    return render_template('form.html', name=personsName)
 
 
 @app.route('/admin/adduser')
@@ -47,19 +25,16 @@ def add_user():
     password = request.form['password']
     fullname = request.form['fullname']
     if username_exists(username):
-        return '<h1>USER ALREADY EXISTS.  TRY A DIFFERENT NAME</h1>'
+        return '<h1>USER ALREADY EXISTS.  <a href="/admin/adduser">TRY A DIFFERENT NAME</a></h1>'
     else:
         add_user_ui(username, password, fullname)
-        data = adminPage()
-        return data
+        return '<h1>User ' + username + 'has been added. <a href="/admin"> HOME</a></h1> '
 
 
 @app.route('/admin/deleteUser/<username>', methods=['POST'])
 def delete_user(username):
     delete_user_ui(username)
-
-    return '<h1>User ' + username + 'deleted. <a href="/admin">HOME</a></h1> '
-
+    return '<h1>User ' + username + 'has been deleted. <a href="/admin"> HOME</a></h1> '
 
 
 @app.route('/admin/edituser/<username>')
@@ -73,15 +48,7 @@ def edit_user():
     password = request.form['password']
     fullname = request.form['fullname']
     edit_user_ui(username, password, fullname)
-    data = adminPage()
-    return data
-
-
-@app.route('/mult', methods=['POST'])
-def mult():
-    x = request.form['x']
-    y = request.form['y']
-    return 'The product is ' + str(int(x) * int(y))
+    return '<h1>User ' + username + 'has been edited. <a href="/admin"> HOME</a></h1> '
 
 
 
