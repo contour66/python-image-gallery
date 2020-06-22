42960d9ba89701acf796e61366fe353d23947a6e
root <root@ip-10-0-1-237.us-west-1.compute.internal>
2020-06-22 01:27:38 +0000

    check

--- ec2-scripts/ec2-prod-1.0.sh
+++ ec2-scripts/ec2-prod-1.0.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/bash
 
 
-export IMAGE_GALLERY_SCRIPT_VERSION="1.1"
-CONFIG_BUCKET="au.zt.image-gallery-config"
+export IMAGE_GALLERY_SCRIPT_VERSION="1.0"
 
 # Install packages
 yum -y update
-yum install -y python3 git postgresql postgresql-devel gcc python3-devel
-amazon-linux-extras install -y nginx1
+yum install -y python3 git
 
 # Configure/install custom software
 cd /home/ec2-user
@@ -15,15 +13,9 @@ git clone https://github.com/contour66/python-image-gallery.git
 chown -R ec2-user:ec2-user python-image-gallery
 su ec2-user -l -c "cd ~/python-image-gallery && pip3 install -r requirements.txt --user"
 
-aws s3 cp s3://${CONFIG_BUCKET}/nginx/nginx.conf /etc/nginx
-aws s3 cp s3://${CONFIG_BUCKET}/nginx/default.d/image_gallery.conf /etc/nginx/default.d
-aws s3 cp s3://${CONFIG_BUCKET}/nginx/index.html /usr/share/nginx/html
-chown nginx:nginx /usr/share/nginx/html/index.html
 
 # Start/enable services
 systemctl stop postfix
 systemctl disable postfix
-systemctl start nginx
-systemctl enable nginx
 
 su ec2-user -l -c "cd ~/python-image-gallery && ./start" >/var/log/image_gallery.log 2>&1 &
