e0491c89f5960a30ac5168a94394e5f54d90503d
root <root@ip-10-0-1-237.us-west-1.compute.internal>
2020-06-22 00:48:25 +0000

    fixed git and bucket links

--- ec2-scripts/ec2-prod-1.1.sh
+++ ec2-scripts/ec2-prod-1.1.sh
@@ -11,7 +11,7 @@ amazon-linux-extras install -y nginx1
 
 # Configure/install custom software
 cd /home/ec2-user
-git clone https://github.com/cdavidshaffer/python-image-gallery.git
+git clone https://github.com/contour66/python-image-gallery.git
 chown -R ec2-user:ec2-user python-image-gallery
 su ec2-user -l -c "cd ~/python-image-gallery && pip3 install -r requirements.txt --user"
 
