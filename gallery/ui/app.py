d70837cbd81088fa494e6d83b25a126646739563
contour66 <zamir.thind@gmail.com>
2020-07-05 13:57:30 -0700

    changed route and upload.html

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -38,7 +38,7 @@ def storage():
 
 
 @app.route("/uploaded", methods=['POST', 'GET'])
-def upload_file():
+def upload_image():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
