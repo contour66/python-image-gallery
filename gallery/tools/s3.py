e3a8a48f181c551641180879e5f47e7fb7b434f9
contour66 <zamir.thind@gmail.com>
2020-07-05 15:51:33 -0700

    ≤

--- gallery/tools/s3.py
+++ gallery/tools/s3.py
@@ -80,7 +80,7 @@ def main():
     list = list_objects('au.zt.image-gallery', 'dog')['Contents']
     # list_objects('au.zt.image-gallery', 'dog')
     for e in list:
-        print(e[0])
+        print(e['Key'])
 
 
 if __name__ == '__main__':
