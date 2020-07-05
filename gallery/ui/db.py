9681550ef30e29ba51341558f9bbddd5b6741701
contour66 <zamir.thind@gmail.com>
2020-07-04 21:39:44 -0700

    cleanup app.py

--- gallery/ui/db.py
+++ gallery/ui/db.py
@@ -74,19 +74,17 @@ def username_exists(username):
     try:
         exists_query = '''
         select exists (
-            select 0,
-                   1,
-                   2
+            select 1,
             from users
             where username = %s
         )'''
         connect()
         cursor = connection.cursor()
         cursor.execute(exists_query, (username,))
-        row = cursor.fetchone()
-        for t in row:
-            print(t[0], t[1], t[2])
-        return row
+        # row = cursor.fetchone()
+        # for t in row:
+        #     print(t[0], t[1], t[2])
+        return cursor.fetchone()[0]
     finally:
         connection.close()
 
