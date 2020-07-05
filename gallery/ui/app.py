38df23263f63420c90247e838d2931fc5628ed5a
contour66 <zamir.thind@gmail.com>
2020-07-05 00:15:01 -0700

    add main

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -64,7 +64,7 @@ def login():
             return redirect('/invalidLogin')
         else:
             session['username'] = request.form["username"]
-            return redirect("/main.html")
+            return redirect('main.html')
     else:
         return render_template('login.html')
 
