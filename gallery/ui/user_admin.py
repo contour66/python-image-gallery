f4ab890e1702afb56cbc383963aa80d7ab5f303d
contour66 <zamir.thind@gmail.com>
2020-07-04 21:48:24 -0700

    cleanup app.py

--- gallery/ui/user_admin.py
+++ gallery/ui/user_admin.py
@@ -141,7 +141,7 @@ def main():
             elif choice == 3:
                 edit_user()
             elif choice == 6:
-                username_exists(dog)
+                username_exists("dog")
             elif choice == 4:
                 delete_user()
             elif choice == 5:
