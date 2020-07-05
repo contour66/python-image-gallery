5cfd6adb3ed483b7f6347a3921d1284a11b7b6c4
contour66 <zamir.thind@gmail.com>
2020-07-05 01:37:32 -0700

    K

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -26,7 +26,7 @@ def upload():
     if request.method == "POST":
         f = request.files['file']
         f.save(os.path.join(UPLOAD_FOLDER, f.filename))
-        upload_file(BUCKET, f"uploads/{f.filename}", )
+        upload_file(BUCKET, f"uploads/{f.filename}", current_user())
 
         return redirect("/storage")
 
