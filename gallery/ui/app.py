e7c42c42da9b81ed2128823e2afc05c94b0cc769
contour66 <zamir.thind@gmail.com>
2020-07-06 12:20:43 -0700

    …

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -46,7 +46,7 @@ def storage():
 
 @app.route("/images")
 def view_images():
-    data = get_objects(BUCKET_NAME, current_user())
+    data = get_object(BUCKET_NAME, current_user())
     # info = data['Contents']['Key']
     return render_template('images.html', image=data)
 
