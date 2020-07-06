af64d639ead05ff76cbf435966935dd0601a3ffe
contour66 <zamir.thind@gmail.com>
2020-07-06 13:14:35 -0700

    …

--- gallery/tools/s3.py
+++ gallery/tools/s3.py
@@ -1,6 +1,9 @@
 import logging
 import boto3
 import json
+from PIL import Image
+from io import BytesIO
+import numpy as np
 from botocore.exceptions import ClientError
 
 
@@ -44,10 +47,12 @@ def get_object(bucket_name, key):
     try:
         s3_client = boto3.client('s3')
         result = s3_client.get_object(Bucket=bucket_name, Key=key)
+        file_stream = result['Body']
+        im = Image.open(file_stream)
     except ClientError as e:
         logging.error(e)
         return None
-    return result
+    return im
 
 
 def put_object(bucket_name, key, value):
