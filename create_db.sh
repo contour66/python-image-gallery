279af9e237ed45545c5a20c56c5b987c99186489
contour66 <zamir.thind@gmail.com>
2020-07-14 21:20:53 -0700

    fixed

--- create_db.sh
+++ create_db.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-
+filename=ig_password
 set -e
 echo "Creating DB"
 psql <<- EOSQL
-    CREATE USER image_gallery WITH PASSWORD ig_password;
+    CREATE USER image_gallery WITH PASSWORD POSTGRES_PASSWORD_FILE=/run/secrets/ig_password;
     CREATE DATABASE image_gallery;
     GRANT ALL PRIVILEGES ON DATABASE image_gallery TO image_gallery;
 EOSQL
